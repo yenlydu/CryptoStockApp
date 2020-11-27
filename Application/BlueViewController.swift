a53846331aa9a592aad7438ca42510e25b2e92c1
Duong Yen-Ly <duong.yenly@yahoo.fr>
2020-11-27 17:16:00 +0100

    added storyboards first basic design, draft profile page, tab bar controller

---
+++ Application/BlueViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BlueViewController.swift
+//  IOSBasicProject
+//
+//  Created by Duong Yen-Ly on 27/11/2020.
+//
+
+import Foundation
+import UIKit
+
+class BlueViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
---
+++ Application/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  IOSBasicProject
+//
+//  Created by Duong Yen-Ly on 27/11/2020.
+//
+
+import Foundation
+
+import UIKit
+
+class ProfileViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Profile"
+        // Do any additional setup after loading the view.
+    }
+
+}
